512e68cf7734d46034e23e2ee44984541ab09f81
jkatsanis <katsanis18@gmail.com>
2023-09-10 07:23:09 +0200

    added some shit i forgo

--- Source/Kernel/Kernel.asm
+++ Source/Kernel/Kernel.asm
@@ -2,16 +2,20 @@
 
 %include "TextRenderer/Print.asm"
 
+; Strings
+
+t_string: db "Hello Josef", 0
+
 kernel_main:
     mov edi, VIDEO_MEM 
-    mov rax, 0x1f201f201f201f201f20
+    mov rax, 0x1f201f201f201f20
     mov ecx, 500
-
     mov [VIDEO_MEM], rax
     rep stosq
 
-    mov al, 'A'
-    call print_char_lm
+    mov esi, t_string
+    call print_string
+
     jmp $
 
 times KERNEL_SIZE+512-($-$$) db 0   ; + 512 bc fucking bootloader
--- Source/Kernel/TextRenderer/Print.asm
+++ Source/Kernel/TextRenderer/Print.asm
@@ -1,10 +1,33 @@
-print_char_lm:
+section .data
+    cursor_x dd 0
+    cursor_y dd 0
+
+
+print_string:
+    lodsb
+    cmp al, 0
+    je done  
+    call print_char
+    inc dword [cursor_x]    
+    jmp print_string
+
+print_char:
     mov edi, VIDEO_MEM  
+    
+    mov ecx, [cursor_y]    
+    mov edx, [cursor_x]      
+    mov ebx, 80 
+    
+    imul ecx, ebx      
+    add ecx, edx       
+    shl ecx, 1 
 
-    mov ah, 0x0F  ; Attribute 0x0F = White text on Black background    
-    ; mov al, 'A'  ; Load the ASCII character 'A' into AL
-
-    mov [VIDEO_MEM], ax
+    add edi, ecx
+    
     stosw
-    ret
+    ret 
 
+done:
+    mov dword [cursor_x], 0
+    inc dword [cursor_y]
+    ret
