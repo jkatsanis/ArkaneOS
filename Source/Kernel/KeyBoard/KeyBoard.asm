ec6a8d401a23e930658e2e79430a664526571e19
jkatsanis <katsanis18@gmail.com>
2023-09-13 17:25:26 +0200

    updatet keyboard driver

--- Source/Kernel/KeyBoard/KeyBoard.asm
+++ Source/Kernel/KeyBoard/KeyBoard.asm
@@ -1,32 +1,53 @@
+section .data
+   map db 0x9E, 65    ; Key: 1, Value: 10  
+
 read_key:
     in al, 0x60
     call check_key
-    cmp rbx, 1
+    cmp bl, 1
     jne read_key
 
-    ; Sending ack flag
-    mov al, 0xFA 
-    out 0x60, al 
+    call send_ack_flag
 
-    mov al, 'A'
     call print_char_line
     ret
 
 check_key:
     call check_a_released
+    check_key_exit:
     ret
 
-    key_return:
-        mov rbx, 1
-        ret
+key_exit:
+    mov bl, 0
+    jmp check_key_exit
 
-    key_exit:
-        mov rbx, 0
-        ret
-
-check_a_released
+check_a_released:
+    mov bl, 1
     cmp al, 0x9E  ; A released
     jne key_exit
-    call key_return
+
+    mov dl, al
+    call find_value
     ret
-        
+
+send_ack_flag:
+    push ax
+    mov al, 0xFA 
+    out 0x60, al 
+    pop ax
+    ret
+
+find_value:
+    mov edi, 0
+    cmp dl, [map + edi]
+    je found
+    add edi, 2
+    ret
+
+found:
+    mov al, [map + edi + 1]
+    ret
+
+not_found:
+    mov al, 0xFF
+
